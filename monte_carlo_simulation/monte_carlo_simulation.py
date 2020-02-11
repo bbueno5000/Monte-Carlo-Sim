052b40408f08c6385aacede7b1e605266496fa2d
B. Bueno <bbueno25@hotmail.com>
2020-02-11 08:47:52 -0700

    modified monte_carlo_simulation.py (Monte Carlo Simulation and Python 2 - Dice Function)

--- monte_carlo_simulation/monte_carlo_simulation.py
+++ monte_carlo_simulation/monte_carlo_simulation.py
@@ -1,7 +1,25 @@
+"""
+DOCSTRING
+"""
 import random
 
 def roll_dice():
-    return random.randint(1, 100)
+    """
+    DOCSTRING
+    """
+    return bool(100 > random.randint(1, 100) > 50)
 
-for count in range(100):
-    print(roll_dice())
+def simple_bettor(bankroll, initial_wager, wager_count):
+    """
+    DOCSTRING
+    """
+    wager = initial_wager
+    for _ in range(wager_count):
+        if roll_dice():
+            bankroll += wager
+        else:
+            bankroll -= wager
+        print('Bankroll:' + str(bankroll))
+
+if __name__ == '__main__':
+    simple_bettor(10000, 100, 100)
