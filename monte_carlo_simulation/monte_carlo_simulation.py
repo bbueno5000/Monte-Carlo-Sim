60401c93b36fbf37088892a4266609ea533c289f
B. Bueno <bbueno25@hotmail.com>
2020-02-11 09:20:00 -0700

    modified monte_carlo_simulation.py (Monte Carlo Simulation and Python 4 - Plotting with Matplotlib)

--- monte_carlo_simulation/monte_carlo_simulation.py
+++ monte_carlo_simulation/monte_carlo_simulation.py
@@ -2,6 +2,7 @@
 DOCSTRING
 """
 import random
+from matplotlib import pyplot
 
 def roll_dice():
     """
@@ -13,14 +14,19 @@ def simple_bettor(bankroll, initial_wager, wager_count):
     """
     DOCSTRING
     """
-    wager = initial_wager
-    for _ in range(wager_count):
+    value, values, wagers = bankroll, [], []
+    for count in range(1, wager_count):
         if roll_dice():
-            bankroll += wager
+            value += initial_wager
         else:
-            bankroll -= wager
-    return bankroll
+            value -= initial_wager
+        wagers.append(count)
+        values.append(value)
+    pyplot.plot(wagers, values)
 
 if __name__ == '__main__':
     for _ in range(100):
-        print('Bankroll:' + str(simple_bettor(10000, 100, 100)))
+        simple_bettor(10000, 100, 100)
+    pyplot.ylabel('Account Value')
+    pyplot.xlabel('Wager Count')
+    pyplot.show()
